93aaba47d61083acf01b7c2095c7cf454d11a261
bezbak <Cemetey.2019@gmail.com>
2025-01-30 23:15:03 +0600

    test

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -7,13 +7,14 @@ import Document, {
     DocumentInitialProps,
   } from "next/document";
   
-  class MyDocument extends Document<{  description: string, image: string }> {
+  class MyDocument extends Document<{  description: string, image: string, title: string }> {
     static async getInitialProps(
       ctx: DocumentContext
-    ): Promise<DocumentInitialProps & { description: string, image: string }> {
+    ): Promise<DocumentInitialProps & { description: string, image: string, title: string }> {
       let description =
         "【uku.kg】 Крупнейший сайт для размещения бесплатных объявлений ➤ Кыргызстан ❱❱❱ 〚Актуальные объявления по темам〛▷ Недвижимость ➦ Транспорт ➦ Электроника ➦ Работа ➦ Услуги ➦ Дом и Сад ➦ Животные ➤ Кыргызстан ᐉ Сервис бесплатных частных и бизнес объявлений от uku.kg!";
       let image = '/image/_logo.png'
+      let title = 'uku.kg'
       try {
         const { id } = ctx.query;
         
@@ -21,26 +22,28 @@ import Document, {
           const res = await fetch(`https://uku.kg/api/v1/publication/${id}/`);
           const data = await res.json();
           image = data.images[0].image
-          console.log(data.images);
+          console.log(data.category);
           description = `${data.description} - ${data.category.name}` || description;
+          title = `${data.title} - ${data.category.name}` || description;
         }
       } catch (error) {
         console.error("Ошибка загрузки данных:", error);
       }
       
       const initialProps = await Document.getInitialProps(ctx);
-      return { ...initialProps, description, image };
+      return { ...initialProps, description, image, title };
     }
   
     render() {
-        const { description, image } = this.props;
+        const { description, image, title } = this.props;
         return (
             <Html lang="ru">
                 <Head>
                     <meta name="description" content={`${description}`} />
                     <meta property="og:image" content={`${image}`} />
                     <meta property="image_src" content={`${image}`} />
-                    <meta property="og:title" content={`${description}`} />
+                    <meta property="og:title" content={`${title}`} />
+                    <meta property="og:description" content={`${description}`}  />
                 </Head>
                 <body>
                     <Main />
