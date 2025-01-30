64fb6813878aefc7184d2388cc5e4b415feac760
bezbak <Cemetey.2019@gmail.com>
2025-01-30 06:31:52 +0600

    add gsg

--- src/components/Layout/index.tsx
+++ src/components/Layout/index.tsx
@@ -10,8 +10,6 @@ export default function Layout({ children, item }: LayoutProps) {
     return (
         <>
             <Head>
-                <title>{item?.description} uku.kg</title>
-                <meta name="description" content="【uku.kg】 Крупнейший сайт для размещения бесплатных объявлений ➤ Кыргызстан ❱❱❱ 〚Актуальные объявления по темам〛▷ Недвижимость ➦ Транспорт ➦ Электроника ➦ Работа ➦ Услуги ➦ Дом и Сад ➦ Животные ➤ Кыргызстан ᐉ Сервис бесплатных частных и бизнес объявлений от uku.kg!" />
                 <meta httpEquiv="X-UA-Compatible" content="IE=edge" />
                 <meta name="google" content="notranslate" />
                 <meta name="keywords" content="новости, лента, обновления" />
