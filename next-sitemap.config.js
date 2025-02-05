b6694088177688b021ea1f7218200249989bc6e2
bezbak <Cemetey.2019@gmail.com>
2025-02-05 12:05:30 +0600

    fasf

--- next-sitemap.config.js
+++ next-sitemap.config.js
@@ -1,6 +1,18 @@
+/** @type {import('next-sitemap').IConfig} */
 module.exports = {
-    siteUrl: 'https://uku.kg',
-    generateRobotsTxt: true,
-    sitemapSize: 5000,
-  };
-  
+  siteUrl: 'https://uku.kg', // Адрес сайта
+  generateRobotsTxt: true,  // Генерация robots.txt
+  sitemapSize: 5000,        // Максимальное количество ссылок в одном файле sitemap
+  changefreq: 'daily',      // Частота обновления страниц (опционально)
+  priority: 0.7,            // Приоритет страниц (опционально)\
+  async additionalPaths(config) {
+    const res = await fetch('https://uku.kg/api/v1/publication/search/?format=json'); // Укажи свой API для получения всех публикаций
+    const data = await res.json();
+
+    // Создаём пути для карты сайта
+    return data.results.map((item) => ({
+      loc: `/detail/${item.id}`, // Путь к странице
+      lastmod: item.updated_at || new Date().toISOString(), // Последнее обновление (если есть)
+    }));
+  },
+};
